114d6306f710a4f26438f3ffa1db7b88cfddf812
PenutChen <penut85420@gmail.com>
2020-05-03 15:51:09 +0800

    Update file structure

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Penut",
-    version="0.0.2",
+    version="0.0.3",
     author="PenutChen",
     author_email="penut85420@gmail.com",
     description="This is a collection of my useful functions.",
@@ -13,6 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/penut85420/Penut",
     packages=setuptools.find_packages(),
+    package_dir = {"penut": "src"},
+    extras_require = {
+        "tf": ["tensorflow>=1.13.1,<2"],
+        "tfgpu": ["tensorflow-gpu>=1.13.1,<2"],
+        "gdown": ["gdown"],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
